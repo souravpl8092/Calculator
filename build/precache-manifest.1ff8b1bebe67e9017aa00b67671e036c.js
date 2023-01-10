36b3d90046175476f650245a2ae07d4aeff29916
Sourav Paul <souravpl8092@gmail.com>
2023-01-10 14:04:38 +0530

    added

---
+++ build/precache-manifest.1ff8b1bebe67e9017aa00b67671e036c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c9d9c80e6d31f604c60",
+    "url": "/static/css/main.b1d66716.chunk.css"
+  },
+  {
+    "revision": "1c9d9c80e6d31f604c60",
+    "url": "/static/js/main.8f89343a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9165e61e5854e3554316",
+    "url": "/static/js/2.ac518772.chunk.js"
+  },
+  {
+    "revision": "a1dd875807b05e2440ca5bdb27f575e4",
+    "url": "/index.html"
+  }
+];
